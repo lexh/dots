2f6327e0cc836e08e5b911795ca83f66e055efdd
Lex Herbert <lherbert@squareup.com>
2021-04-15 06:38:21 -0700

    stash this monstrosity

---
+++ emacs.d/init.el
@@ -0,0 +1,177 @@
+(setq inhibit-startup-message t)
+(setq-default indent-tabs-mode nil)
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(tooltip-mode -1)
+(set-fringe-mode 10)
+
+;; Whitespace
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+(menu-bar-mode -1)
+(column-number-mode)
+(global-display-line-numbers-mode t)
+(setq visible-bell t)
+
+(set-face-attribute 'default nil :font "Source Code Pro" :height 170)
+
+;; Package sources
+(require 'package)
+(setq package-archives '(
+			 ("melpa" . "https://melpa.org/packages/")
+			 ("org" . "https://orgmode.org/elpa/")
+			 ("elpa" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+(use-package counsel
+  :config
+  (counsel-mode 1))
+
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+(use-package ivy
+  :diminish
+  :bind (("C-s" . swiper)
+         :map ivy-minibuffer-map
+         ("TAB" . ivy-alt-done)
+         ("C-l" . ivy-alt-done)
+         ("C-j" . ivy-next-line)
+         ("C-k" . ivy-previous-line)
+         :map ivy-switch-buffer-map
+         ("C-k" . ivy-previous-line)
+         ("C-l" . ivy-done)
+         ("C-d" . ivy-switch-buffer-kill)
+         :map ivy-reverse-i-search-map
+         ("C-k" . ivy-previous-line)
+         ("C-d" . ivy-reverse-i-search-kill))
+  :config
+  (ivy-mode 1))
+
+(use-package all-the-icons)
+
+(use-package doom-modeline
+  :init (doom-modeline-mode 1)
+  :custom ((doom-modeline-height 15)))
+
+(use-package doom-themes
+  :init (load-theme 'doom-dracula t))
+
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config
+  (setq which-key-idle-delay 0.3))
+
+(use-package ivy-rich
+  :init
+  (ivy-rich-mode 1))
+
+(use-package counsel
+  :bind (("M-x" . counsel-M-x)
+	 ("C-x b" . counsel-ibuffer)
+	 ("C-x C-f" . counsel-find-file)
+	 :map minibuffer-local-map
+	 ("C-r" . counsel-minibuffer-history)))
+
+(use-package helpful
+  :commands (helpful-callable helpful-variable helpful-command helpful-key)
+  :custom
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describe-variable-function #'helpful-variable)
+  :bind
+  ([remap describe-function] . counsel-describe-function)
+  ([remap describe-command] . helpful-command)
+  ([remap describe-variable] . counsel-describe-variable)
+  ([remap describe-key] . helpful-key))
+
+(use-package general
+  :config
+  (general-create-definer lexh/leader-keys
+    :keymaps '(normal insert visual emacs)
+    :prefix "SPC"
+    :global-prefix "C-SPC"))
+
+(use-package evil
+  :init
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  (setq evil-want-C-u-scroll t)
+  (setq evil-want-C-i-jump nil)
+  :config
+  (evil-mode 1)
+  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
+  (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
+  (define-key evil-motion-state-map (kbd "<home>") 'evil-first-non-blank)
+
+  ;; Use visual line motions even outside of visual-line-mode buffers
+  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+
+  (evil-set-initial-state 'messages-buffer-mode 'normal)
+  (evil-set-initial-state 'dashboard-mode 'normal))
+
+(use-package evil-collection
+  :after evil
+  :config
+  (evil-collection-init))
+
+(with-eval-after-load 'calendar
+  (evil-collection-calendar-setup))
+
+(use-package org
+  :hook (
+         (org-mode . (lambda ()
+                      (visual-line-mode 1)
+                      (flyspell-mode 1)))
+         (before-save . (lambda () (org-update-statistics-cookies "ALL"))))
+  :config
+  (setq org-ellipsis " ▾")
+  ;; this isn't working as expected, only works in insert mode
+  (setq org-return-follows-link 1)
+  (setq org-link-abbrev-alist
+        '(("jira" . "https://jira.sqprod.co/browse/%s")))
+  (setq org-enforce-todo-checkbox-dependencies 1)
+  (setq org-todo-keywords
+        '((sequence "TODO" "ACTIVE" "WAIT" "|" "DONE")))
+  (setq org-todo-keyword-faces
+        '(("TODO" . org-warning)
+          ("ACTIVE" . "pink")
+          ("WAIT" . "yellow")
+          ("DONE" . org-done))
+  ))
+
+(use-package flycheck
+  :init (global-flycheck-mode))
+
+(use-package evil-org
+  :after org
+  :hook (org-mode . (lambda () evil-org-mode))
+  :config
+  (require 'evil-org-agenda)
+  (evil-org-agenda-set-keys))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(global-command-log-mode t)
+ '(package-selected-packages
+   '(evil-org flycheck elisp-autofmt evil-magit magit hydra evil-collection evil general doom-themes helpful ivy-rich which-key rainbow-delimiters counsel doom-modeline ivy command-log-mode use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
